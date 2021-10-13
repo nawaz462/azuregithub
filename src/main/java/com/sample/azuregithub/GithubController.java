2853635af8bcf74b33c18cf15c31382f3a6e3986
Shahbaz Syed <shahbaz_syed@epam.com>
2021-10-13 15:57:01 +0530

    Exception test

--- src/main/java/com/sample/azuregithub/GithubController.java
+++ src/main/java/com/sample/azuregithub/GithubController.java
@@ -1,5 +1,6 @@
 package com.sample.azuregithub;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
+@Slf4j
 public class GithubController {
 
     private static List<Student> studentList = new ArrayList<>();
@@ -18,8 +20,14 @@ public class GithubController {
     }
 
     @GetMapping
-    public List<Student> getStudents(){
-        return studentList;
+    public List<Student> getStudents() throws Exception {
+        log.info("Get All Students..");
+        studentList.forEach(student ->{
+            System.out.println("ID: "+ student.getId());
+            System.out.println("NAME: "+ student.getName());
+        });
+        throw new Exception("Exception test");
+//        return studentList;
     }
 
     @PostMapping
